119bbad68c1b9abcc3ac6360d363de0618048592
fbzavaleta <benjamin1992.1992@gmail.com>
2021-04-24 23:34:33 -0300

    Release2404- usart

--- atmega32U4/main.c
+++ atmega32U4/main.c
@@ -3,10 +3,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
+
 /*
 https://create.arduino.cc/editor/ProfeGarcia/94423536-718b-49c8-8268-de791fae0b6d/preview
 */
-
+ /*
+ https://www.youtube.com/watch?v=cAui6116XKc
+ */
 
 void setup_RGB()
 {
@@ -21,6 +24,30 @@ void setup_RGB()
 
 }
 
+void usart_init(unsigned int baud)
+{
+    /*Baud rate*/
+    UBRR1H = (unsigned char)(baud >> 8);
+    UBRR1L = (unsigned char)baud;
+
+    /*Activando el receptor/transmisor*/
+    UCSR1B = (1<<RXEN1) | (1<<TXEN1);
+    UCSR1C = (1<<USBS1) | (3<<UCSZ10);
+}
+
+void usart_trasnmit(unsigned char data)
+{
+    while (!(UCSR1A & (1<< UDRE1))){}
+
+    UDR1 = data;
+    
+}
+
+void usart_recive( void)
+{
+    while(UCSR1A & (1<< RXC1)){}
+    return UDR1;
+}
 
 
 int main(int argc)
