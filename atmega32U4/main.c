c7dfd5f1cf8b8b11529b40626b796933791aff37
fbzavaleta <benjamin1992.1992@gmail.com>
2021-06-10 22:42:19 -0300

    Release1005- Captura por interrupciones y timer delay

--- atmega32U4/main.c
+++ atmega32U4/main.c
@@ -1,37 +1,29 @@
 #include "VirtualSerial.h"
 #include "main.h"
-
-
-
 /*
 calculador de timer:
 https://eleccelerator.com/avr-timer-calculator/
 */
 
-char teste [] = "Teste final 2\r\n";
-char msg_1 [] = "Setado A\r\n";
-int period;
-
-int cicle = 0;
-int flag_status=1;
-int flag_cicle = 0;
-
-int Rojo_Frec = 0;
+/*
+S0  --> PB7 (PIN)
+S1  --> PB6
+S3  --> PB0
+S2  --> PB4
+OUT --> PB2
+*/
+int logic = 0;
+int Rojo_Frec  = 0;
 int Verde_Frec = 0;
-int Azul_Frec = 0;
+int Azul_Frec  = 0;
 
 void RGB_Setup()
 {
-    //Configurando salidas 
-    DDRB |= (S0 | S3 | S2) ;
-    DDRE |=  S1;
+    //Configurando salidas y entradas
+    DDRB = 0b11010001;
 
-    //Configurando entradas
-    DDRD &= (SOUT ^ 0xFF);
-
-    //S0 y S1 en estado alto
-    PORTB |= S0;
-    PORTE |= S1;
+    //Configurando las frecuencias en 20% s0 high s1 low
+    PORTB = 0b01000000;
 }
 
 void COUNTER_setup(int t_ticks)
@@ -40,80 +32,88 @@ void COUNTER_setup(int t_ticks)
 
     TCCR0A = (1 << WGM01); //set CTC: cuando numero de ticks math, genera una interrupcion pag106
     OCR0A  = t_ticks; // El valor total de ticks que sera comparado, pag 109
-    TIMSK0 = (1 << OCIE0A); //checkea la interrupcion pag 109
+    TIMSK0 = (1 << OCIE0A); //habilita la interrupcion pag 109
     sei();
-    TCCR0B = (1<<CS02) | (1<<CS00); //preescaler en 1024  pag 108
+    TCCR0B = (1<<CS02) | (1<<CS00); //preescaler en 1024  pag 108 
 }
 
-/*
-La variable se tiene que cerada en otra funcion
-externa, nunca en la misma funcion de interrupciones
-*/
-void precise_delay()
+void PIN_Interrup_setup()
 {
-if(flag_cicle ==1)
-{
-flag_cicle=0;
-return; 
+    PCICR = (1<<PCIE0); // Habilitando change interrupt control pag91 11.1.5
+    PCMSK0 = (1 << PCINT2);//HAbilitando el PB2 pag 91 11.1.7
+    //PORTB |= 0b00000100;
+    sei(); // HAbilitando globalmente las interrupciones
+    
 }
+
+void Precise_delay(int seconds)
+{
+    /*
+    Para un overflow de 10 ms
+    TCTNT0 = 255 - CPUFREQ * delya/preescaleer
+    TCTNT0 =255 - 16MHz * 10ms/1024 ~= 100 
+    */
+    TCNT0 = 100;
+    TCCR0B = (1<<CS02) | (1<<CS00); //preescaler en 1024  pag 108 
+
+    int counter = 0;
+
+    while ((counter*10) < seconds)
+    {
+        while ((TIFR0 & 0x01) == 0x00); // caso ocurra una interrupcion
+        TIFR0 = 0x01;
+        counter++;
+    }
+    
+}
+
+ISR(PCINT0_vect)
+{
+    logic=1;
 }
 
 int main()
 {
     RGB_Setup();
     USB_Init_Handle();
-    COUNTER_setup(156); //intervalos de 0.01 segundos(ciclo de máquina)
+    PIN_Interrup_setup();
     
     char sensor_salida[8];
 
     do
-    {
+    {   
+        sprintf(sensor_salida,"%i\n", logic);
+        //USB_Device_write_Com("sensor_salida\n");
 
-        switch (flag_status)
-        {
-        case 1:
-            PORTB &= ( (S2 | S3) ^ 0xFF); // Setando S2 y S3 en LOW
-            sprintf(sensor_salida,"%i\n",(PIND & 0x40));
-            USB_Device_write_Com(sensor_salida);
+        PORTB &= 0b11101110; //s2 low s3 low
+        USB_Device_write_Com("Activado1\n");
+        USB_Device_write_Com(sensor_salida);
+        Precise_delay(10);
 
-            break;
+        PORTB &= ((1 << PB4) | (1 << PB0)); //s2 y s3 high
+        USB_Device_write_Com("Activado2\n");
+        USB_Device_write_Com(sensor_salida);
+        Precise_delay(10);
 
-        case 2:
-            USB_Device_write_Com("segundo\n");
-
-            break;
-        
-        default:
-            break;
-        }
-        EVENT_USB_Device_Release();
-        
-        precise_delay();
+        PORTB |= 0b00000001; // s2 low s3 high
+        USB_Device_write_Com("Activado3\n");
+        USB_Device_write_Com(sensor_salida);
+        Precise_delay(10);
 
     } while (1);
         
     
 }
 
-
-
+/*
 ISR(TIMER0_COMPA_vect)
 {
     cicle++;
 
     if (cicle > 1000) //0.01s * 500 = 5 segundo
     {
-        if (flag_status == 1)
-        {
-            flag_status = 2;
-        }
-        else if (flag_status == 2)
-        {
-            flag_status = 1;
-        }
-        
         cicle = 0;
-        flag_cicle = 1;
     }
 
 }
+*/
